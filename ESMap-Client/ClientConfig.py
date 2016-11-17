8bceae590d72592603cb41f0d875d84016a1ada5
Davnit <davnit31@gmail.com>
2016-11-16 21:53:03 -0500

    Remove TestValue from config

--- ESMap-Client/ClientConfig.py
+++ ESMap-Client/ClientConfig.py
@@ -18,7 +18,6 @@ def save(c, path=None):
 
     p = configparser.ConfigParser()
     p.add_section('MAIN')
-    p.set('MAIN', 'test', c.TestValue)
     if c.SourceLocation is not None: p.set('MAIN', 'source_location', c.SourceLocation)
     p.set('MAIN', 'tick_interval', str(c.TickInterval))
 
@@ -26,7 +25,6 @@ def save(c, path=None):
         p.write(file)
 
 class Config():
-    def __init__(self, p):
-        self.TestValue = p.get('MAIN', 'test', fallback='default value')            
+    def __init__(self, p):         
         self.SourceLocation = p.get('MAIN', 'source_location', fallback=None)       # Path to local file containing source metadata
         self.TickInterval = int(p.get('MAIN', 'tick_interval', fallback="60"))        # Time to wait between checking if sources need to be updated
