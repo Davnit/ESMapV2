b079d988edf7ec11137c686bdd48569c86175c54
Davnit <davnit31@gmail.com>
2016-11-23 00:20:43 -0500

    Rework config sections
    There are now 2 sections, 'main' and 'paths'. Main contains general
    program settings. Paths contains URLs and file locations.
    
    Some new settings were also added to determine if the client should
    connect to a remote server and the locations of that servers resources.
    
    When the program loads, if no config exists, a default one will be
    created and saved.

--- ESMap-Client/ClientConfig.py
+++ ESMap-Client/ClientConfig.py
@@ -1,32 +1,46 @@
 
-import configparser, io
+import configparser
+from os import path
 
 defaultConfigPath = 'config.ini'
 
-def load(path=None):
-    if path is None:
-        path = defaultConfigPath
+def load(configPath=None):
+    if configPath is None:
+        configPath = defaultConfigPath
+
+    # Try to parse the config file
+    p = configparser.ConfigParser()
+    p.read(configPath)
+    c = Config(p)
+
+    # If the file doesn't exist, create it with default values
+    if not path.isfile(configPath):
+        save(c, configPath)
+    return c
+
+def save(c, configPath=None):
+    if configPath is None:
+        configPath = defaultConfigPath
 
     p = configparser.ConfigParser()
-    p.read(path)
 
-    return Config(p)
+    p.add_section("MAIN")
+    p.set("MAIN", "tick_interval", str(c.TickInterval))
+    p.set("MAIN", "use_remote", str(c.UseRemoteServer))
 
-def save(c, path=None):
-    if path is None:
-        path = defaultConfigPath
+    p.add_section("PATHS")
+    p.set("PATHS", "sources", c.SourceLocation)
+    p.set("PATHS", "ingest", c.IngestUrl)
+    p.set("PATHS", "sync", c.SyncUrl)
 
-    p = configparser.ConfigParser()
-    p.add_section('MAIN')
-    if c.SourceLocation is not None: p.set('MAIN', 'source_location', c.SourceLocation)
-    p.set('MAIN', 'tick_interval', str(c.TickInterval))
-    p.set('MAIN', 'ingest_url', c.IngestUrl)
-
-    with open(path, 'w') as file:
+    with open(configPath, "w") as file:
         p.write(file)
 
 class Config():
-    def __init__(self, p):         
-        self.SourceLocation = p.get('MAIN', 'source_location', fallback=None)       # Path to local file containing source metadata
-        self.TickInterval = int(p.get('MAIN', 'tick_interval', fallback="60"))      # Time to wait between checking if sources need to be updated
-        self.IngestUrl = p.get('MAIN', 'ingest_url', fallback=None)                 # URL for the script that receives call data
+    def __init__(self, p):
+        self.TickInterval = int(p.get("MAIN", "tick_interval", fallback="60"))      # Time to wait between checking if sources need to be updated
+        self.UseRemoteServer = bool(p.get("MAIN", "use_remote", fallback=False))    # Should the client sync with a remote server?
+      
+        self.SourceLocation = p.get("PATHS", "sources", fallback="sources.txt")     # Location to obtain sources from
+        self.IngestUrl = p.get("PATHS", "ingest", fallback="")                      # URL to report call list changes to
+        self.SyncUrl = p.get("PATHS", "sync", fallback="")                          # URL to get a list of active calls
