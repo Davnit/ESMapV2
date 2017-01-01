24ec541ee859b2a60bafb7ff3bef7ddf4f98cc9e
Davnit <davnit31@gmail.com>
2017-01-01 16:30:11 -0500

    Move log.php to list.php

--- Web/header.php
+++ Web/header.php
@@ -2,8 +2,8 @@
             <h1>Emergency Services Activity - Orange County, FL</h1>
             <div id="menu">
                 <ul>
-                    <li><a href="index.php">Live Map</a></li>
-                    <li><a href="log.php">Call List</a></li>
+                    <li><a href="./index.php">Live Map</a></li>
+                    <li><a href="./list.php">Call List</a></li>
                 </ul>
             </div>
         </div>
