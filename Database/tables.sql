3b94ff47e690751c55b85ec453da607466e6b341
Davnit <davnit31@gmail.com>
2017-01-05 05:00:50 -0500

    Verify location bounds before adding to map
    This ensures that for sources with designated boundaries, only calls
    mapped to geocodes that are within their source's boundary will be
    dispalyed. This helps reduce miscoded locations. There isn't really a
    way to do this on the geocode level since a geocode could be used for
    calls from multiple sources, though calls caught by this should be
    flagged for review (future).

--- Database/tables.sql
+++ Database/tables.sql
@@ -21,6 +21,7 @@ CREATE TABLE `sources` (
   `url` varchar(255) CHARACTER SET ascii NOT NULL,
   `parser` varchar(25) CHARACTER SET ascii NOT NULL,
   `interval` int(11) NOT NULL,
+  `bounds` varchar(45) CHARACTER SET ascii NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Web/generate.php
+++ Web/generate.php
@@ -3,14 +3,13 @@
     require_once "database.php";
     
     // Get a list of sources
-    $sourceList = getData("SELECT id, tag FROM sources");
+    $sourceList = getData("SELECT id, tag, bounds FROM sources");
     
-    // Turn it into a lookup
-    $sources = array();
-    foreach ($sourceList as $sL)
-    {
-        $sources[intval($sL["id"])] = $sL["tag"];
-    }
+    // Get a lookup of source ID -> tag
+    $tags = array_column($sourceList, "tag", "id");
+    
+    // Get a lookup of source ID -> bounds[], where each element of the array is a lat or lng (ne_lat, ne_lng, sw_lat, sw_lng)
+    $bounds = array_map(function ($x) { return explode("|", str_replace(",", "|", $x)); }, array_column($sourceList, "bounds", "id"));    
     
     // Get list of active or recently expired calls.
     $sql = "SELECT c.source, c.meta, c.added, c.expired, g.latitude, g.longitude FROM calls c ";
@@ -22,16 +21,38 @@
     $tableCalls = array();
     foreach ($callList as $cL)
     {
+        $src = intval($cL["source"]);
         $meta = json_decode($cL["meta"]);
         
         // The live map should only contain calls that have resolved coordinates.
         if (($cL["latitude"] != null) and ($cL["longitude"] != null))
         {
-            $mapCalls[] = array(
-                "desc" => sprintf("%s @ %s", $meta->description, $meta->location),
-                "lat" => floatval($cL["latitude"]),
-                "lng" => floatval($cL["longitude"]),
-            );
+            $verified = true;
+            
+            // Check if the source of this call has a defined boundary
+            if (array_key_exists($src, $bounds))
+            {
+                $bnd = $bounds[$src];
+                
+                $fLat = floatval($cL["latitude"]);
+                $fLng = floatval($cL["longitude"]);
+                
+                // Check if the point is outside of the bounds
+                if ($fLat > $bnd[0] and $fLng > $bnd[1] and $fLat < $bnd[2] and $fLng < $bnd[3])
+                {
+                    $verified = false;
+                }
+            }
+            
+            if ($verified)
+            {
+                // Add the point to the map list
+                $mapCalls[] = array(
+                    "desc" => sprintf("%s @ %s", $meta->description, $meta->location),
+                    "lat" => $fLat, 
+                    "lng" => $fLng
+                );
+            }
         }
         
         // Determine the call's start time. If the source specified a time, use that, otherwise
@@ -40,7 +61,7 @@
         
         // The call log table contains slightly more information about every call.
         $tableCalls[] = array(
-            "dept" => intval($cL["source"]),    # ID of the call source
+            "dept" => $src,                     # ID of the call source
             "desc" => $meta->description,       # Call description
             "loc" => $meta->location,           # Unprocessed location of the call
             "time" => $callTime,                # The time the call was made or found
@@ -58,7 +79,7 @@
     // Create the object for the call list and serialize it.
     $obj = array(
         "updated" => time(),
-        "sources" => $sources,
+        "sources" => $tags,
         "calls" => $tableCalls
     );
     file_put_contents("data/call_log.json", json_encode($obj, JSON_PRETTY_PRINT));
