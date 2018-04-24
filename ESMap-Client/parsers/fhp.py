26a2836f57b6e7094e36ae5ace07705343c7d9dc
Davnit <davnit31@gmail.com>
2018-04-24 06:07:51 -0400

    [Client] Update FHP parser for new site

--- ESMap-Client/parsers/fhp.py
+++ ESMap-Client/parsers/fhp.py
@@ -1,95 +1,91 @@
 
 '''
     Web page parsing script for 'Florida Highway Patrol' (FHP)
-    http://www.flhsmv.gov/fhp/traffic/crs_h407.htm
+    https://www.flhsmv.gov/fhp/traffic/live_traffic_feed.html
     
     At this time, this script will only return calls from Orange County.
+    Updated to support the new ARCGIS system.
 '''
 
-def getRowValue(table, name):
-    return table.split(name)[1].split("<td")[1].split(">", 1)[1].split("</td>")[0].replace("&nbsp;", " ").strip()
+dataObj = json.loads(data)
 
-replacements.update({
-    "TURNPIKE": "FLORIDA TURNPIKE", "TPKE": "FLORIDA TURNPIKE",
-    "GREENWAY": "FL-417", "GREENEWAY": "FL-417"
-})
-
-types = {
-    "Police": [ "SILVER ALERT", "AMBER ALERT", "ROCK THROWING", "MISSING PERSON", "POSSIBLE FATALITY" ],
-    "Death": [ "SUICIDE", "FATALITY" ]
-}
-
-infoList = data.split("<input type=\"hidden\" id=\"popInfo")    
-for idx in range(1, len(infoList)):
+for incident in dataObj["features"]:
     try:
-        info = infoList[idx].split("value=\"")[1]
-
-        row_data = { }
-            
-        meta = { }
-        meta["call_number"]   = getRowValue(info, "Incident #")
-        meta["dispatched"]    = getRowValue(info, "Dispatched")
-        meta["arrived"]       = getRowValue(info, "Arrived")    
-        meta["call_date"]     = getRowValue(info, "Date")
-            
-        # FHP doesn't provide unique call numbers so we need to come up with our own
-        cd = meta["call_date"].split("/")
-        row_data["key"] = '-'.join([ "FHP", ''.join([ cd[2], cd[0], cd[1] ]), meta["call_number"][9:] ])
+        attr = incident["attributes"]
     
-        location = getRowValue(info, "Location").rsplit("[", 1)
-        meta["location"]      = location[0].replace("  ", " ").strip()
+        loc = attr["LOCATION"].rsplit("[", 1)
+        desc = attr["TYPEEVENT"]
         
-        if len(location) > 1:
-            meta["city"]      = location[1].split("]")[0].strip().title()
-   
-        meta["county"]        = getRowValue(info, "County").title()
-        meta["remarks"]       = getRowValue(info, "Remarks")
-        meta["description"]   = getRowValue(info, "Incident Type")
-        
-        call_type = "Traffic"
-        desc = meta["description"].upper()
-        if desc.startswith("FIRE"):
-            call_type = "Fire"
+        # The city is designated in brackets at the end of the location.
+        if len(loc) > 1:
+            city = loc[1].split("]")[0].strip().title()
         else:
-            for sType, termList in types.items():
-                for term in termList:
-                    if term in desc:
-                        call_type = sType
-            
-        # Make a rough parse of the location string
-        #   This source is pretty good about providing its own geocodes but this is just in case.
-        loc = meta["location"]
+            city = ""
         
-        # Check for cross streets (usually always provided, even when it's a direct address)
-        if (" x[" in loc):
-            parts = loc.split(" x[")
-            loc = parts[0] + " AND " + parts[1][:-1]
-        else:
-            loc = loc
+        meta = {
+            "call_number": attr["INCIDENTID"],
+            "call_date": attr["DATE"],
+            "dispatched": attr["DISPATCHTIME"],
+            "arrived": attr["ARRIVETIME"],
+            "location": loc[0].replace("  ", " ").strip(),
+            "county": attr["COUNTY"].title(),
+            "city": city,
+            "remarks": attr["REMARKS"],
+            "description": desc
+        }
         
-        # If the first "word" is a number this is most likely a direct address
-        if loc.split()[0].isdigit():
-            loc = loc.split("[")[0]
+        # Use the dispatch or arrival time for the call time, if specified.
+        if len(meta["dispatched"]) > 0:
+            meta["call_time"] = meta["call_date"] + " " + meta["dispatched"]
+        elif len(meta["arrived"]) > 0:
+            meta["call_time"] = meta["call_date"] + " " + meta["arrived"]
+        
+        location = meta["location"]
+        if " x[" in location:
+            parts = location.split(" x[")
+            location = ""
             
-        # Replace SR- (state road) prefix with the state FL
-        loc = loc.replace("SR-", "FL-")
+            # Attempt to sort out the location string. FHP provides cross streets, mile markers, and other details
+            #  some of which can be redundant. This makes a best effort to normalize the locations to match others.
+            for i in range(0, len(parts) - 1):
+                if " [" in parts[i]:
+                    sub = parts[i].split(" [")
+                    if sub[0] == sub[1].split("]")[0]:
+                        location += sub[0]
+                    else:
+                        location += parts[i]
+                else:
+                    location += parts[i]
+                location += " AND "
             
-        row_data["location"]  = loc
-        row_data["category"]  = call_type
-        row_data["meta"]      = meta
+            # If the first component of the location is a number it's probably a direct address.
+            if location.split()[0].isdigit():
+                location = location.split("[")[0]
+                
+            # Replace generic "SR" (state road) prefix with the state "FL"
+            location = location.replace("SR-", "FL-")
+        
+        category = "Traffic"
+        if desc.upper().startswith("FIRE"):
+            category = "Fire"
+        elif "ALERT" in desc.upper():
+            category = "Alert"
+           
+        row_data = {
+            "key": attr["INCIDENTID"],
+            "category": category,
+            "location": meta["location"],
+            "geo_lat": attr["LAT"],
+            "geo_lng": attr["LON"],
+            "meta": meta
+        }
+        
+         
         
         results.append(row_data)
     except Exception as ex:
-        print("Error parsing 'FHP' index {0}: {1}".format(idx, ex))
-
-# Parse source coordinate data
-latcol = data.split("var Lat = ")[1].split("[")[1].split("];")[0].replace("\"", "").split(",")    
-lngcol = data.split("var Lng = ")[1].split("[")[1].split("];")[0].replace("\"", "").split(",")
-if ((len(latcol) == len(results)) and (len(lngcol) == len(results))):
-    for idx in range(0, len(results)):
-        if (not latcol[idx].startswith("0")) and (not lngcol[idx].startswith("0")):
-            results[idx]["geo_lat"] = latcol[idx]
-            results[idx]["geo_lng"] = lngcol[idx]
+        print("Error parsing 'FHP': {0}".format(ex))
+        
 
 # Only add calls from Orange County
 results[:] = [ item for item in results if (item["meta"]["county"].upper() == "ORANGE") ]
